66e1dfa2af67a11b4e02c5aca5ba5d2a522956c6
orcham <orcham49@gmail.com>
2023-03-21 15:32:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_ x 10(void);
+void print_alphabet_, * 10(void)
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,7 +12,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
 void print_times_table(int n);
 
 
