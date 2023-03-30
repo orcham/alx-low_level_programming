ad0a81a2d9f55130721348863458bb17aab1a9d9
orcham <orcham49@gmail.com>
2023-03-30 13:52:56 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ *
+ */
+
+void print_number(int n)
+
+{
+
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+	putchar('_');
+	num = -num;
+	}
+
+	if ((num / 10) > 0)
+	print_number(num / 10);
+	putchar((num % 10) + '0');
+}
