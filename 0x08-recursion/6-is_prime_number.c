ce687ade4e1236985df4c51d7c613e11a29ecc9c
orcham <orcham49@gmail.com>
2023-04-05 05:31:42 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,6 +7,17 @@ int check_prime(int n, int i);
 /**
  * is_prime_number - Returns if a number is prime
  * @n: the number to be checked
+ *
+ * Return: integer value
+ */
+int is_prime_numer(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+ * check_prime - Check if number is prime
+ * @n: the number to be checked
  * @i: the iteration times
  *
  * Return: 1 for prime or 0 composite
